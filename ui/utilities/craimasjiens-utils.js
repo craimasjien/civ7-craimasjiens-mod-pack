e06eeb7c4c33262d06ababa6008644a3aeccda6a
Bryan Sam Meulmeester <1733692+craimasjien@users.noreply.github.com.>
2025-02-12 13:26:49 +0100

    Optimized getPlotHasCity and moved it to it's own utilities file.

---
+++ ui/utilities/craimasjiens-utils.js
@@ -0,0 +1,37 @@
+
+export function getCityOnPlot(location) {
+
+}
+
+export function getPlotHasCity(location, playerID) {
+    const thisCity = Cities.getAtLocation(location);
+
+    if (thisCity.location.x == location.x && thisCity.location.y == location.y) {
+        return true;
+    }
+
+    return false;
+
+
+    const player = Players.get(playerID);
+    const playerCities = player.Cities;
+
+    for (const cityID of playerCities.getCityIds()) {
+        const city = Cities.get(cityID);
+
+        if (city.location.x == location.x && city.location.y == location.y) {
+            return true;
+        }
+    }
+
+    return false;
+}
+
+export const HexToFloat4 = (hex, alpha = 1) => {
+    const r = (hex >> 16) & 0xff;
+    const g = (hex >> 8) & 0xff;
+    const b = hex & 0xff;
+    return { x: r / 255, y: g / 255, z: b / 255, w: Math.min(1, Math.max(0, alpha)) };
+};
+
+export const DISCOVERY_COLOR = HexToFloat4(0x24D618, .6);
