e1e369007476279ad5edcd1ceb9348a5a1798a16
Bryan Sam Meulmeester <1733692+craimasjien@users.noreply.github.com.>
2025-02-12 15:32:22 +0100

    All is good, now on to showing the plots for other cities.

--- ui/lenses/layer/religion-layer.js
+++ ui/lenses/layer/religion-layer.js
@@ -1,64 +1,60 @@
 import LensManager from '/core/ui/lenses/lens-manager.js';
 
+import { ReligionColors } from '/craimasjiens-mod-pack/utilities/craimasjiens-utils.js';
+
 class ReligionLensLayer {
     constructor() {
         this.religionOverlayGroup = WorldUI.createOverlayGroup("ReligionOverlayGroup", 1);
         this.religionOverlay = this.religionOverlayGroup.addPlotOverlay();
         this.religionPlots = [];
     }
+
     clearOverlay() {
         this.religionOverlayGroup.clearAll();
         this.religionOverlay.clear();
         this.religionPlots = [];
     }
+
     initLayer() {
     }
+
     applyLayer() {
         this.clearOverlay();
-
-        const cities = [];
-
+    
         const width = GameplayMap.getGridWidth();
         const height = GameplayMap.getGridHeight();
-        for (let x = 0; x < width; x++) {
-            for (let y = 0; y < height; y++) {
-                const location = { x: x, y: y };
-
-                const thisCity = Cities.getAtLocation(location);
-
-                if (!thisCity)
-                    continue;
-
-                cities.push(thisCity);
-            }
-        }
-
+    
+        const cities = Array.from({ length: width }, (_, x) =>
+            Array.from({ length: height }, (_, y) => Cities.getAtLocation({ x, y }))
+        )
+        .flat()
+        .filter(Boolean)
+        .filter((value, index, self) => self.indexOf(value) === index);
+    
         const religions = new Map();
         const religionsPlotsMap = new Map();
-
-        cities.forEach((city) => {
+    
+        cities.forEach(city => {
             const religion = this.getReligionForCity(city);
-            if (!religion)
-                return;
+            if (!religion) return;
             
-            religions.set(religion.Name, religion);
+            console.error("City "+ city.name + " has major religion " + religion.Name);
+    
+            religions.set(religion.ReligionType, religion);
 
-            const plotLocations = [];
-            const cityPlots = city.getPurchasedPlots();
-            for (let plotIndex = 0; plotIndex < cityPlots.length; plotIndex++) {
-                const plot = GameplayMap.getLocationFromIndex(cityPlots[plotIndex]);
+            const plotLocations = city.getPurchasedPlots().map(plotIndex =>
+                GameplayMap.getLocationFromIndex(plotIndex)
+            );
 
-                plotLocations.push(plot);
-            }
-
-            religionsPlotsMap.set(religion.Name, plotLocations);
+            console.error(JSON.stringify(plotLocations));
+    
+            religionsPlotsMap.set(religion.ReligionType, plotLocations);
         });
-
-        religionsPlotsMap.forEach((religionPlots, key) => {
-            const religion = religions.get(key);
-            this.religionOverlay.addPlots(religionPlots, { fillColor: religion.Color });
+    
+        religionsPlotsMap.forEach((plots, religionType) => {
+            this.religionOverlay.addPlots(plots, { fillColor: ReligionColors[religionType] });
         });
-    }
+    }   
     
     removeLayer() {
         this.clearOverlay();
@@ -66,28 +62,6 @@ class ReligionLensLayer {
 
     getReligionForCity(aCity) {
         return GameInfo.Religions.find(t => t.$hash == aCity.Religion?.majorityReligion);
-    }
-
-    assertReligion(location) {
-        var religions = [];
-
-        const thisCity = Cities.getAtLocation(location);
-
-        if (!thisCity)
-            return religions;
-
-        const religion = GameInfo.Religions.find(t => t.$hash == thisCity?.Religion?.majorityReligion);
-
-        if (!religion)
-            return religions;
-
-        const cityPlots = thisCity.getPurchasedPlots();
-        for (let plotIndex = 0; plotIndex < cityPlots.length; plotIndex++) {
-            const plot = GameplayMap.getLocationFromIndex(plotIndex);
-
-            religions[religion.Name] = []
-        }
-        return religions;
     }
 }
 LensManager.registerLensLayer('cmp-religion-layer', new ReligionLensLayer());
